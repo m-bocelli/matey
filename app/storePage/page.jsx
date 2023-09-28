8271251eb25a2414b9188a93603dda9b1070251b
Christopher Bennett <cbcolleg@udel.edu>
2023-09-28 16:48:06 -0400

    added css

--- app/storePage/page.jsx
+++ app/storePage/page.jsx
@@ -1,19 +1,17 @@
 import {Card, CardBody, CardHeader} from 'react-bootstrap'
+import './storeStyles.css';
 
 export default function StorePageUI() {
     return (
-        <section> 
-
-            <h1>storePage</h1>
-
+        <section className='outsideBackground'> 
             <Card>
-                <CardHeader>All Pets</CardHeader>
+                <CardHeader style={{height: "4rem", fontSize: "x-large"}}>All Pets</CardHeader>
                 <CardBody>
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <h5 className="card-title">Card title</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
                 </CardBody>
             </Card>
         </section>
