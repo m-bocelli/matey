14f24bba06c3abd4c67c3f312ec723ae4be54798
Ethan S <estipes@udel.edu>
2023-09-30 17:44:16 -0400

    Added basic framework to createHouse and manageHouses pages

--- app/createHouse/page.jsx
+++ app/createHouse/page.jsx
@@ -1,3 +1,15 @@
 export default function CreateHouseUI() {
-    return <h1> Create House </h1>
+    return (
+        <>
+        <h1> Create House </h1>
+        <form id="createHouse">
+            <label>House Name:</label>
+            <input type="text" id="houseName"></input><br></br>
+            <label>House Maties:</label>
+            <input type="text" id="houseMate1"></input>
+            <input type="submit" value="submit"></input>
+        </form>
+        <button></button>
+        </>
+    )
 }
