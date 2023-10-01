6a9682938529a4dbcaf3263b981dbcc74a25f47c
Ethan S <estipes@udel.edu>
2023-10-01 19:19:31 -0400

    Testing to see if random key generator works and made create and join pages accessible

--- app/manageHouses/page.jsx
+++ app/manageHouses/page.jsx
@@ -3,8 +3,8 @@ export default function ManageHousesUI() {
     return (
         <div>
             <h1> Manage Houses </h1>
-            <button class="newHouse">Create House</button>
-            <button class="newHouse">Join House</button>
+            <button class="newHouse" onclick="window.open(createHouse)">Create House</button>
+            <button class="newHouse" onclick="window.open(joinHouse)">Join House</button>
         </div>
     )
 }
