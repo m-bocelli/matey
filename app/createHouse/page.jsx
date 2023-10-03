c9da1a29a757855ef2e281d03a39260d28e7ada3
Ethan S <estipes@udel.edu>
2023-10-02 20:24:30 -0400

    The functionality of creating and joining house works with localStorage for only one house at a time currently and added option to Delete a house

--- app/createHouse/page.jsx
+++ app/createHouse/page.jsx
@@ -1,21 +1,44 @@
-export function createHouse(){
-    let key = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    for (let i = 0; i < 12; i++){
-        key += characters.charAt(Math.floor(Math.random() * characters.length))
+'use client';
+function createHouse(e){
+    e.preventDefault();
+    let hName = document.getElementById("houseName").value;
+    if(hName == null || hName == ""){
+        alert("Please enter a house name");
     }
-    console.log(key);
-    window.open("dashboard");
+    else{
+        let key = "";
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        for (let i = 0; i < 12; i++){
+            key += characters.charAt(Math.floor(Math.random() * characters.length))
+        }
+        let u = {
+            houseName: hName,
+            houseMates: ["creatorId"]
+        };
+        let uL = JSON.stringify(u);
+        localStorage.setItem(key, uL);
+        let a = JSON.parse(localStorage.getItem("keyList"));
+        let b = [];
+        if(a != null){
+            a[a.length] = key;
+            localStorage.setItem("keyList", JSON.stringify(a));
+        } else{
+            b[0] = key;
+            localStorage.setItem("keyList", JSON.stringify(b));
+        }
+        alert("Your house key is " + key);
+        location.href = "dashboard";
+    }
 }
 
 export default function CreateHouseUI() {
     return (
         <div>
             <h1> Create House </h1>
-            <form id="createHouse">
+            <form id="createHouse" onSubmit={createHouse}>
                 <label>House Name:</label>
                 <input type="text" id="houseName" name="houseName"></input><br></br>
-                <input type="submit" value="submit" onclick="createHouse()"></input>
+                <input type="submit" value="submit"></input>
                 {/*Submit button onclick will use a function to genrate a unique House Key*/}
             </form>
         </div>
