ce83b979d1d13c43cbc4c44586251b348a145c98
Mikey <michael.j.bocelli@gmail.com>
2023-10-04 22:24:17 -0400

    feat: sign-out component that presents users with buttons to sign in when signed-out

--- app/components/AquariumPet/AquariumPet.jsx
+++ app/components/AquariumPet/AquariumPet.jsx
@@ -1,14 +1,13 @@
 import styles from './AquariumPet.module.css';
 import Button from '../Button/Button';
 
-
-
 function generateRandomNumber(min, max) {
-    return Math.floor((Math.random() * (max - min + 1) + min));
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomDirection(rng) {
-    if(rng==1) return "left"; else return "right";
+    if (rng == 1) return 'left';
+    else return 'right';
 }
 
 export default function AquariumPet({ name, img, user }) {
@@ -16,11 +15,20 @@ export default function AquariumPet({ name, img, user }) {
 
     return (
         <div className={styles.item_container}>
-            <marquee behaviour="scroll" direction={randomDirection(rng)} scrollamount={generateRandomNumber(10, 20)}>            
-                <div>    
-                    {name} 
-                    ({user.split(' ')[0]})       
-                    <img className={rng===1 ? styles.left : styles.fishTank} src={img} alt="Fish Picture" width="50" height="50"/>
+            <marquee
+                behaviour='scroll'
+                direction={randomDirection(rng)}
+                scrollamount={generateRandomNumber(10, 20)}
+            >
+                <div>
+                    {name}({user.split(' ')[0]})
+                    <img
+                        className={rng === 1 ? styles.left : styles.fishTank}
+                        src={img}
+                        alt='Fish Picture'
+                        width='50'
+                        height='50'
+                    />
                 </div>
             </marquee>
         </div>
