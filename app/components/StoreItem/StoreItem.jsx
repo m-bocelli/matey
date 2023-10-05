ca2af4eb0834a71715cd98f44306803f6567369c
Mikey <michael.j.bocelli@gmail.com>
2023-10-04 23:38:17 -0400

    change: made marketplace resemble tf2 screenshot

--- app/components/StoreItem/StoreItem.jsx
+++ app/components/StoreItem/StoreItem.jsx
@@ -1,19 +1,19 @@
 import styles from './StoreItem.module.css';
 import Button from '../Button/Button';
 
-
-export default function StoreItem({ name, desc, price, img }) {
+export default function StoreItem({
+    name,
+    desc,
+    price,
+    img,
+    onClick,
+    isSelected,
+}) {
     return (
-        <div className={styles.item_container}>
-            <div>🐙</div>
-            <div className={styles.item_blurb}>
-                <p>{name}</p>
-                <p>{desc}</p>
-            </div>
-            <div className={styles.item_buy_sect}>
-                {price}
-                <Button>BUY</Button>
-            </div>
+        <div className={styles.item_container} onClick={onClick}>
+            <img className={styles.item_img} src={img}></img>
+            <p className={styles.name}>{name}</p>
+            <div className={styles.item_buy_sect}>{price}</div>
         </div>
     );
 }
