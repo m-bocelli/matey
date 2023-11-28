3b3bdedd7175cccc17172bd856fa9bd5222bb23a
Christopher Bennett <cbcolleg@udel.edu>
2023-11-28 09:15:40 -0500

    Made the fish move

--- client/app/components/AquariumPet/AquariumPet.jsx
+++ client/app/components/AquariumPet/AquariumPet.jsx
@@ -1,6 +1,6 @@
 import styles from './AquariumPet.module.css';
 import Button from '../Button/Button';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,9 +12,10 @@ function randomDirection(rng) {
 }
 
 function setFishPosition(x, y) {
-
-
+    setX(x);
+    setY(y);
 }
+
 
 export default function AquariumPet({ name, img, user }) {
     let rng = generateRandomNumber(1, 2);
@@ -22,17 +23,29 @@ export default function AquariumPet({ name, img, user }) {
     const [x,setX] = useState(0);
     const [y, setY] = useState(0);
 
+    const [time, setTime] = useState(Date.now());
+
+    useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 100);
+    return () => {
+        console.log("fish is: " + x + " , " + y);
+        clearInterval(interval);
+        setX(x + 2);
+    };
+    }, [time]);
+
+
     return (
         <div className={styles.item_container}>
                 <div>
                     {name}({user.split(' ')[0]})
                     <img
-                        className={rng === 1 ? styles.left : styles.fishTank}
+                        className={styles.fishTank}
                         src={img}
                         alt='Fish Picture'
                         width='50'
                         height='50'
-                        style={{left: x}}
+                        style={{transform: `translate(${x}px, 100%)`}}
                     />
                 </div>
         </div>
